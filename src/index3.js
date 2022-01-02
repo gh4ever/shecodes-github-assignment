b34263258a1c0cf58590ec898efb9c1eca0ac216
gh4ever <leonedoro@naver.com>
2022-01-02 13:02:22 +0200

    everything updated

--- src/index3.js
+++ src/index3.js
@@ -1,60 +1,71 @@
 function formatDate(date) {
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let dayIndex = date.getDay();
+  let hours = date.getHours()
+  if (hours < 10) {
+    hours = `0${hours}`
+  }
+  let minutes = date.getMinutes()
+  if (minutes < 10) {
+    minutes = `0${minutes}`
+  }
+  let dayIndex = date.getDay()
   let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[dayIndex];
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ]
+  let day = days[dayIndex]
 
-  return `${day} ${hours}:${minutes}`;
+  return `${day} ${hours}:${minutes}`
 }
 
 function showWeather(response) {
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#tempnow").innerHTML = Math.round(
-    response.data.main.temp
-  );
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = response.data.wind.speed;
-  document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
+  document.querySelector('#city').innerHTML = response.data.name
+  document.querySelector('#tempnow').innerHTML = Math.round(
+    response.data.main.temp,
+  )
+  document.querySelector('#humidity').innerHTML = response.data.main.humidity
+  document.querySelector('#wind').innerHTML = response.data.wind.speed
+  document.querySelector('#description').innerHTML =
+    response.data.weather[0].main
 }
 
 function searchCity(city) {
-  let myKey = `384b4ddb18472833708d25e324b56156`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${myKey}`;
+  let myKey = `384b4ddb18472833708d25e324b56156`
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${myKey}`
 
-  axios.get(apiUrl).then(showWeather);
+  axios.get(apiUrl).then(showWeather)
 }
 
 function changeName(event) {
-  event.preventDefault();
-  let targetName = document.querySelector("#city");
-  targetName.innerHTML = document.querySelector("#cityname").value;
-  searchCity(city);
+  event.preventDefault()
+  let targetName = document.querySelector('#city')
+  targetName.innerHTML = document.querySelector('#cityname').value
+  searchCity(targetName.innerHTML)
 }
 
 function showLocation(position) {
-  let myKey = `384b4ddb18472833708d25e324b56156`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${myKey}&units=metric`;
-  axios.get(apiUrl).then(showWeather);
+  let myKey = `384b4ddb18472833708d25e324b56156`
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${myKey}&units=metric`
+  axios.get(apiUrl).then(showWeather)
 }
 
-let dateElement = document.querySelector("#date");
-let currentTiem = new Date();
-dateElement.innerHTML = formatDate(currentTiem);
+function getCurrentLocation(event) {
+  event.preventDefault()
+  navigator.geolocation.getCurrentPosition(showLocation)
+}
 
-let searchForm = document.querySelector("#search-bar");
-searchForm.addEventListener("submit", changeName);
+let dateElement = document.querySelector('#date')
+let currentTiem = new Date()
+dateElement.innerHTML = formatDate(currentTiem)
 
-let currentLocation = document.querySelector("#current-button");
-currentLocation.addEventListener("click", showLocation);
+let searchForm = document.querySelector('#search-bar')
+searchForm.addEventListener('submit', changeName)
 
-searchCity("Tel Aviv");
+let currentLocation = document.querySelector('#current-button')
+currentLocation.addEventListener('click', getCurrentLocation)
+
+searchCity('Tel Aviv')
