376c5c592097a8b5ff054d6e343ed1a3b437e12b
gh4ever <leonedoro@naver.com>
2022-01-15 22:14:54 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -39,34 +39,49 @@ function showDateTime(timestamp) {
   return `${date.getFullYear()}/${month}/${date.getDate()} ${hour}:${minutes}`
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000)
+  let day = date.getDay()
+  let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+
+  return days[day]
+}
+
 function showForecast(response) {
+  let forecast = response.data.daily
+
   let forecastElement = document.querySelector('#forecast')
 
-  let days = ['Thu', 'Fri', 'Sat', 'Sun']
-
   let forecastHTML = `<div class="row">`
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
       <div class="col-2">
-        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
         <img
-          src="http://openweathermap.org/img/wn/50d@2x.png"
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
           alt=""
           width="42"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> 18° </span>
-          <span class="weather-forecast-temperature-min"> 12° </span>
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max,
+          )}˚C</span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min,
+          )}˚C</span>
         </div>
       </div>
-  `
+      `
+    }
   })
 
   forecastHTML = forecastHTML + `</div>`
   forecastElement.innerHTML = forecastHTML
-  console.log(forecastHTML)
 }
 
 function getForecast(coordinates) {
@@ -99,7 +114,8 @@ function showTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
   )
   iconElement.setAttribute(alt, response.data.weather[0], description)
-  getForecast(response.data.coords)
+
+  getForecast(response.data.coord)
 }
 
 function handleSubmit(event) {
@@ -157,4 +173,3 @@ let currentLocation = document.querySelector('#location')
 currentLocation.addEventListener('click', getCurrentLocation)
 
 search('Tel Aviv')
-showForecast('Tel Aviv')
