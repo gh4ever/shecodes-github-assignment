5a5040b120011712880bb1600e17451d1b2d0ce9
gh4ever <leonedoro@naver.com>
2022-01-15 23:44:26 +0200

    description fixed

--- src/index.js
+++ src/index.js
@@ -52,14 +52,17 @@ function showForecast(response) {
 
   let forecastElement = document.querySelector('#forecast')
 
-  let forecastHTML = `<div class="row">`
+  let forecastHTML = `<div class="row" id="forecast-week">`
+
   forecast.forEach(function (forecastDay, index) {
     if (index < 6) {
       forecastHTML =
         forecastHTML +
         `
       <div class="col-2">
-        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <div class="weather-forecast-date" id="forecast-day">${formatDay(
+          forecastDay.dt,
+        )}</div>
         <img
           src="http://openweathermap.org/img/wn/${
             forecastDay.weather[0].icon
@@ -68,9 +71,9 @@ function showForecast(response) {
           width="42"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> ${Math.round(
+          <span class="weather-forecast-temperature-max"><strong>${Math.round(
             forecastDay.temp.max,
-          )}˚C</span>
+          )}˚C</strong></span>
           <span class="weather-forecast-temperature-min"> ${Math.round(
             forecastDay.temp.min,
           )}˚C</span>
@@ -85,7 +88,6 @@ function showForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates)
   let apiKey = '384b4ddb18472833708d25e324b56156'
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid={apiKey}&units=metric`
   console.log(apiUrl)
@@ -113,7 +115,7 @@ function showTemperature(response) {
     'src',
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
   )
-  iconElement.setAttribute(alt, response.data.weather[0], description)
+  iconElement.setAttribute(alt, response.data.weather[0].description)
 
   getForecast(response.data.coord)
 }
