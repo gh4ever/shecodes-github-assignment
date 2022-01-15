0516fdd827d4d8575b227960445729df254e13db
gh4ever <leonedoro@naver.com>
2022-01-16 00:17:00 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -52,7 +52,7 @@ function showForecast(response) {
 
   let forecastElement = document.querySelector('#forecast')
 
-  let forecastHTML = `<div class="row">`
+  let forecastHTML = `<div class="row" id="forecast">`
 
   forecast.forEach(function (forecastDay, index) {
     if (index > 0 && index < 6) {
