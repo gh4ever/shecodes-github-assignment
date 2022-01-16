dff825757b6b36ea544ef7ff27d1633062e9d815
gh4ever <leonedoro@naver.com>
2022-01-16 10:58:29 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -69,9 +69,9 @@ function showForecast(response) {
           width="42"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max">${Math.round(
+          <span class="weather-forecast-temperature-max"><strong>${Math.round(
             forecastDay.temp.max,
-          )}˚</span>
+          )}˚</strong></span>
           <span class="weather-forecast-temperature-min"> ${Math.round(
             forecastDay.temp.min,
           )}˚</span>
